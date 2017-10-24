83017f1169ce10b093b84aee9a4e834a4f77bd34
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2017-10-24 15:50:15 -0300

    added missing module

---
+++ sample-app-java-x11/selenium-hello/src/test/java/br/com/example/DuckDuckGoTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.io.File;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.OutputType;
+
+public class DuckDuckGoTest {
+ 
+	private WebDriver driver;
+	private static final String URL = "file://" + new File("./res/public/index.html").getAbsolutePath();
+	public final By searchFieldSelectorBy = By.id("search_field");
+    public final By searchButtonSelectorBy = By.id("search_button");
+
+
+	@Test
+	public void shouldDoASearchWithSuccess() throws Exception{
+		System.setProperty("webdriver.gecko.driver", "./res/geckodriver");
+		driver = new FirefoxDriver();
+		 
+		driver.get(URL);
+
+		WebElement searchField = driver.findElement(searchFieldSelectorBy);
+        searchField.clear();
+        searchField.sendKeys("Darth Vader");
+
+        WebDriver augmentedDriver = new Augmenter().augment(driver);
+		File file = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
+		 
+		WebElement searchButton = driver.findElement(searchButtonSelectorBy);
+		searchButton.click();
+		 
+		driver.getPageSource().contains("Darth Vader");
+		 
+		driver.quit();
+	}
+}
