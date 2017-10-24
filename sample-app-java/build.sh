657b0d434613de0bfdf9d2ff3e4f982789077dbd
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2017-10-24 15:10:40 -0300

    Rename folder

--- sample-app-java/build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t fulano .
-docker run -ti --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/home/ fulano firefox
--- sample-app-java/configure.sh
+++
@@ -1,5 +0,0 @@
-rm -rf .git
-git init
-
-git config user.name "gogs"
-git config user.email "gogs@email.com"
